3a838384c4048d92c1b0003ba40703ab4d0231c1
salyarka <salyarka@gmail.com>
2018-03-30 15:43:13 +0300

    Added, listening of postgres channel.

--- asy.py
+++ asy.py
@@ -1,9 +1,17 @@
 import asyncio
 import functools
 import signal
+import os
+import select
 
+from psycopg2 import connect
 from datetime import datetime
 
+from postgres_access import PostgresAccess
+
+
+# TODO: exit programm when catch signal SIGTERM
+# go away from select
 
 async def lala():
     try:
@@ -21,6 +29,20 @@ async def lolo():
             await asyncio.sleep(5)
     except asyncio.CancelledError:
         print('Task lolo canceled!')
+
+async def catch_notify():
+    try:
+        with PostgresAccess(pg_conn) as db:
+            db.execute('LISTEN task;')
+            while True:
+                if select.select([pg_conn], [], [], 5) == ([], [], []):
+                    continue
+                pg_conn.poll()
+                while pg_conn.notifies:
+                    notification = pg_conn.notifies.pop()
+                    print('got notification', notification)
+    except asyncio.CancelledError:
+        print('Task catch_notify canceled!')
 
 
 async def exit():
@@ -43,11 +65,18 @@ for signame in ('SIGINT', 'SIGTERM'):
         functools.partial(asyncio.ensure_future, exit())
     )
 
-asyncio.ensure_future(lala(), loop=loop)
-asyncio.ensure_future(lolo(), loop=loop)
+#asyncio.ensure_future(lala(), loop=loop)
+#asyncio.ensure_future(lolo(), loop=loop)
+asyncio.ensure_future(catch_notify(), loop=loop)
 
-try:
-    loop.run_forever()
-finally:
-    loop.close()
+if __name__ == '__main__':
+    pg_uri = os.getenv('PG_URI')
+    if pg_uri is None:
+        print('Environment variable PG_URI is not defined!!!')
+        exit(1)
+    pg_conn = connect(pg_uri)
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
 
