10b16ccaf01645ca2cb7482446276050516f8804
salyarka <salyarka@gmail.com>
2018-04-24 17:37:46 +0300

    Modified, using asyncio.Queue instead of asyncio.Event.

--- asy.py
+++ asy.py
@@ -8,54 +8,39 @@ from psycopg2 import connect
 from postgres_access import AsyncPostgresAccess
 
 
-# TODO:
-#       - redesign all variables, ???go away from global???
-#       - make wrapper around Event (for using events by workers)
+# TODO: RuntimeWarning: coroutine 'stop' was never awaited
 
-
-deferred_tasks = []
-
-async def catch_notify():
+async def catch_notify(queue, conn):
     try:
-        with AsyncPostgresAccess(listener_conn, loop) as db:
+        loop = asyncio.get_event_loop()
+        with AsyncPostgresAccess(conn, loop) as db:
             await db.listen('task')
             while True:
                 notifications = await db.get_notifications()
-                print('!!! notifications', notifications)
+                print('Got notifications from Postgres', notifications)
                 for n in notifications:
-                    for ev in evs:
-                        if not ev.is_set():
-                            ev.data = n
-                            ev.set()
-                            break
-                    else:
-                        print('!!! put task to deferred')
-                        global deferred_tasks
-                        deferred_tasks += notifications
+                    queue.put_nowait(n)
     except asyncio.CancelledError:
-        print('Task catch_notify canceled!')
+        pass
 
-async def worker(ev, conn):
+
+async def do_work(queue, conn, number):
     try:
+        loop = asyncio.get_event_loop()
         while True:
-            await ev.wait()
-            print('!!! worker receive %s' % (ev.data,))
+            notification = await queue.get()
+            print('Worker %s receive %s' % (number, notification))
             with AsyncPostgresAccess(conn, loop) as db:
                 res = await db.execute('SELECT pg_sleep(5);', result=True)
-                print('!!! query result', res)
-            print('!!! worker finish')
-            ev.clear()
-            try:
-                task = deferred_tasks.pop(0)
-                print('!!! Worker get deferred task %s' % (task,))
-            except IndexError:
-                print('!!! deferred tasks are empty')
+                print('Query result', res)
+            print('Worker %s finish' % (number,))
+            queue.task_done()
     except asyncio.CancelledError:
-        print('Worker canceled!')
+        pass
 
 
 async def stop():
-    # TODO: RuntimeWarning: coroutine 'exit' was never awaited
+    loop = asyncio.get_event_loop()
     tasks = [
         t for t in asyncio.Task.all_tasks()
         if t is not asyncio.tasks.Task.current_task()
@@ -66,31 +51,41 @@ async def stop():
     loop.stop()
 
 
-loop = asyncio.get_event_loop()
+if __name__ == '__main__':
+    settings = {
+        'PG_URI': os.getenv('PG_URI'),
+        'WORKERS_NUM': os.getenv('WORKERS_NUM')
+    }
+    for k, v in settings.items():
+        if v is None:
+            print('Environment variable %s is not defined!!!' % (k,))
+            exit(1)
 
-evs = [asyncio.Event() for each in range(10)]
+    try:
+        workers_num = int(settings['WORKERS_NUM'])
+    except ValueError:
+        print('Environment variable WORKERS_NUM must be an integer!!!')
+        exit(1)
 
-for signame in ('SIGINT', 'SIGTERM'):
-    loop.add_signal_handler(
-        getattr(signal, signame),
-        functools.partial(asyncio.ensure_future, stop())
-    )
+    listener_conn = connect(settings['PG_URI'], async=True)
+    workers_conns = [
+        connect(settings['PG_URI'], async=True)
+        for each in range(workers_num)
+    ]
+    event_loop = asyncio.get_event_loop()
+    q = asyncio.Queue()
 
+    for signame in ('SIGINT', 'SIGTERM'):
+        event_loop.add_signal_handler(
+            getattr(signal, signame),
+            functools.partial(asyncio.ensure_future, stop())
+        )
 
-# if __name__ == '__main__':
-pg_uri = os.getenv('PG_URI')
-if pg_uri is None:
-    print('Environment variable PG_URI is not defined!!!')
-    exit(1)
-# pg_conn = connect(pg_uri)
-listener_conn = connect(pg_uri, async=True)
-workers_conns = [connect(pg_uri, async=True) for each in range(10)]
+    for i in range(workers_num):
+        asyncio.ensure_future(do_work(q, workers_conns[i], i), loop=event_loop)
+    asyncio.ensure_future(catch_notify(q, listener_conn), loop=event_loop)
 
-for i in range(10):
-    asyncio.ensure_future(worker(evs[i], workers_conns[i]), loop=loop)
-asyncio.ensure_future(catch_notify(), loop=loop)
-
-try:
-    loop.run_forever()
-finally:
-    loop.close()
+    try:
+        event_loop.run_forever()
+    finally:
+        event_loop.close()
